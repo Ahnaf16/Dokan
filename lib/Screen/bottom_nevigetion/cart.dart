09b06b42e6b90c894949a1bbe90e912bde89a78e
Ahnaf Sakil <ahnafsakil9@gmail.com>
2021-12-21 02:34:58 +0600

    added products grid view

--- lib/Screen/bottom_nevigetion/cart.dart
+++ lib/Screen/bottom_nevigetion/cart.dart
@@ -1,19 +1,60 @@
-import 'package:dokan/Properties/export.dart';
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
 
-class Cart extends StatelessWidget {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dokan/Properties/export.dart';
+
+class Cart extends StatefulWidget {
   const Cart({Key? key}) : super(key: key);
 
   @override
+  State<Cart> createState() => _CartState();
+}
+
+class _CartState extends State<Cart> {
+  getProdacts() async {
+    try {
+      QuerySnapshot qn = await _firestore.collection('Products').get();
+      setState(() {
+        for (int i = 0; i < qn.docs.length; i++) {
+          products.add({
+            "product-name": qn.docs[i]["name"],
+            "product-price": qn.docs[i]["price"],
+            "product-img": qn.docs[i]["img"],
+          });
+        }
+      });
+    } on Exception catch (e) {
+      print(e.toString());
+    }
+  }
+
+  final _firestore = FirebaseFirestore.instance;
+
+  List products = [];
+  List<String> loading = [
+    'https://c.tenor.com/tEBoZu1ISJ8AAAAC/spinning-loading.gif',
+    'loading'
+  ];
+
+  @override
+  void initState() {
+    getProdacts();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: AppColor.appBackground,
       body: Center(
-        child: Text(
-          'Cart',
-          style: AppTextStyle.headerStyle,
-        ),
-      ),
+          child: Column(
+        children: [
+          Text(
+            'Cart',
+            style: AppTextStyle.headerStyle,
+          ),
+        ],
+      )),
     );
   }
 }
--- lib/Screen/bottom_nevigetion/homepage.dart
+++ lib/Screen/bottom_nevigetion/homepage.dart
@@ -16,12 +16,6 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   //
 
-  int dotPosition = 0;
-
-  List<String> imgForSlider = [];
-
-  final _firestore = FirebaseFirestore.instance;
-
   getSliderImgs() async {
     try {
       QuerySnapshot qn = await _firestore.collection('Slider').get();
@@ -37,10 +31,39 @@ class _HomepageState extends State<Homepage> {
     }
   }
 
+  getProdacts() async {
+    try {
+      QuerySnapshot qn = await _firestore.collection('Products').get();
+      setState(() {
+        for (int i = 0; i < qn.docs.length; i++) {
+          products.add({
+            "product-name": qn.docs[i]["name"],
+            "product-price": qn.docs[i]["price"],
+            "product-img": qn.docs[i]["img"],
+          });
+        }
+      });
+    } on Exception catch (e) {
+      print(e.toString());
+    }
+  }
+
+  final _firestore = FirebaseFirestore.instance;
+
+  int dotPosition = 0;
+
+  List<String> imgForSlider = [];
+  List products = [];
+  List<String> loading = [
+    'https://c.tenor.com/tEBoZu1ISJ8AAAAC/spinning-loading.gif',
+    'loading'
+  ];
+
   @override
   void initState() {
-    super.initState();
     getSliderImgs();
+    getProdacts();
+    super.initState();
   }
 
   @override
@@ -48,48 +71,120 @@ class _HomepageState extends State<Homepage> {
     return Scaffold(
       backgroundColor: AppColor.appBackground,
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 20,
-            ),
-            child: Column(
-              children: [
-                Material(
-                  elevation: 15,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Material(
+                elevation: 15,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 10,
+                      ),
+                      child: CarouselSlider.builder(
+                        itemCount: imgForSlider.length,
+                        options: CarouselOptions(
+                          height: 135,
+                          autoPlay: true,
+                          enlargeCenterPage: true,
+                          viewportFraction: .8,
+                          onPageChanged: (index, reason) =>
+                              setState(() => dotPosition = index),
+                        ),
+                        itemBuilder: (context, index, realIndex) {
+                          final showImg = imgForSlider.isEmpty
+                              ? loading[index]
+                              : imgForSlider[index];
+                          return buildImg(showImg, index);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: buildIndicator(),
+                    ),
+                  ],
+                ),
+              ),
+              cDivider(30),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Text(
+                  'Featured Products',
+                  style: AppTextStyle.bodyTextStyle,
+                ),
+              ),
+              cDivider(10),
+              productGrid(),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Text(
+                  'All Products',
+                  style: AppTextStyle.bodyTextStyle,
+                ),
+              ),
+              productGrid(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Expanded productGrid() {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1, childAspectRatio: 1),
+          itemCount: products.isEmpty ? 1 : products.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusDirectional.circular(15),
+                  side: BorderSide(color: AppColor.appMainColor, width: 2)),
+              elevation: 10,
+              child: SizedBox(
+                //color: Colors.grey,
+                height: 200,
+                width: 200,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 10,
-                          vertical: 10,
-                        ),
-                        child: CarouselSlider.builder(
-                          itemCount: imgForSlider.length,
-                          options: CarouselOptions(
-                            height: 170,
-                            autoPlay: true,
-                            enlargeCenterPage: true,
-                            viewportFraction: 1,
-                            onPageChanged: (index, reason) =>
-                                setState(() => dotPosition = index),
-                          ),
-                          itemBuilder: (context, index, realIndex) {
-                            final showImg = imgForSlider[index];
-                            return buildImg(showImg, index);
-                          },
-                        ),
+                      AspectRatio(
+                        aspectRatio: 2,
+                        child: Image.network(products.isEmpty
+                            ? loading[0]
+                            : products[index]["product-img"][0]),
                       ),
                       Padding(
-                        padding: const EdgeInsets.only(bottom: 10),
-                        child: buildIndicator(),
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: Text(
+                          '${products.isEmpty ? loading[1] : products[index]["product-name"]}',
+                          style: AppTextStyle.bodyTextStyle,
+                        ),
                       ),
+                      Text(
+                        '${products.isEmpty ? loading[1] : products[index]["product-price"]}',
+                        style: AppTextStyle.bodyTextStyle,
+                      )
                     ],
                   ),
                 ),
-              ],
-            ),
-          ),
+              ),
+            );
+          },
         ),
       ),
     );
@@ -103,25 +198,18 @@ class _HomepageState extends State<Homepage> {
             color: AppColor.appMainColor,
             width: 2,
           ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 5,
-            vertical: 5,
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image.network(
+          image: DecorationImage(
+            image: NetworkImage(
               showImg,
-              fit: BoxFit.fitWidth,
             ),
+            fit: BoxFit.fitWidth,
           ),
         ),
       );
 
   Widget buildIndicator() => AnimatedSmoothIndicator(
         activeIndex: dotPosition,
-        count: imgForSlider.length,
+        count: imgForSlider.isEmpty ? 1 : imgForSlider.length,
         effect: SwapEffect(
           activeDotColor: AppColor.appMainColor,
           dotColor: AppColor.appSecColor,
