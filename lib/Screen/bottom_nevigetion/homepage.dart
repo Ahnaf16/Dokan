b1fb30aed414b8514850809229f37447ecd8b5dd
Ahnaf Sakil <ahnafsakil9@gmail.com>
2021-12-24 15:06:11 +0600

    used future builder to create product list and slider

--- lib/Screen/bottom_nevigetion/homepage.dart
+++ lib/Screen/bottom_nevigetion/homepage.dart
@@ -2,7 +2,6 @@
 
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import '../../Properties/export.dart';
 
@@ -16,55 +15,19 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   //
 
-  getSliderImgs() async {
-    try {
-      QuerySnapshot qn = await _firestore.collection('Slider').get();
-      setState(() {
-        for (int i = 0; i < qn.docs.length; i++) {
-          imgForSlider.add(
-            qn.docs[i]['img'],
-          );
-        }
-      });
-    } on Exception catch (e) {
-      print(e.toString());
-    }
-  }
-
-  getProdacts() async {
-    try {
-      QuerySnapshot qn = await _firestore.collection('Products').get();
-      setState(() {
-        for (int i = 0; i < qn.docs.length; i++) {
-          products.add({
-            "product-name": qn.docs[i]["name"],
-            "product-price": qn.docs[i]["price"],
-            "product-img": qn.docs[i]["img"],
-          });
-        }
-      });
-    } on Exception catch (e) {
-      print(e.toString());
-    }
-  }
-
   final _firestore = FirebaseFirestore.instance;
 
-  int dotPosition = 0;
-
-  List<String> imgForSlider = [];
-  List products = [];
-  List<String> loading = [
-    'https://c.tenor.com/tEBoZu1ISJ8AAAAC/spinning-loading.gif',
-    'loading'
-  ];
-
-  @override
-  void initState() {
-    getSliderImgs();
-    getProdacts();
-    super.initState();
+  Future getSliderImg() async {
+    QuerySnapshot qn = await _firestore.collection('Slider').get();
+    return qn.docs;
   }
+
+  Future getProdacts() async {
+    QuerySnapshot qn = await _firestore.collection('Products').get();
+    return qn.docs;
+  }
+
+  int dotPosition = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -72,64 +35,13 @@ class _HomepageState extends State<Homepage> {
       backgroundColor: AppColor.appBackground,
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: 20,
-          ),
+          padding: const EdgeInsets.only(top: 20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Material(
-                elevation: 15,
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 10,
-                        vertical: 10,
-                      ),
-                      child: CarouselSlider.builder(
-                        itemCount: imgForSlider.length,
-                        options: CarouselOptions(
-                          height: 135,
-                          autoPlay: true,
-                          enlargeCenterPage: true,
-                          viewportFraction: .8,
-                          onPageChanged: (index, reason) =>
-                              setState(() => dotPosition = index),
-                        ),
-                        itemBuilder: (context, index, realIndex) {
-                          final showImg = imgForSlider.isEmpty
-                              ? loading[index]
-                              : imgForSlider[index];
-                          return buildImg(showImg, index);
-                        },
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 10),
-                      child: buildIndicator(),
-                    ),
-                  ],
-                ),
-              ),
+              imgSlider(),
               cDivider(30),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: Text(
-                  'Featured Products',
-                  style: AppTextStyle.bodyTextStyle,
-                ),
-              ),
-              cDivider(10),
-              productGrid(),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: Text(
-                  'All Products',
-                  style: AppTextStyle.bodyTextStyle,
-                ),
-              ),
-              productGrid(),
+              productList(),
             ],
           ),
         ),
@@ -137,84 +49,189 @@ class _HomepageState extends State<Homepage> {
     );
   }
 
-  Expanded productGrid() {
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: GridView.builder(
-          scrollDirection: Axis.horizontal,
-          shrinkWrap: true,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 1, childAspectRatio: 1),
-          itemCount: products.isEmpty ? 1 : products.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadiusDirectional.circular(15),
-                  side: BorderSide(color: AppColor.appMainColor, width: 2)),
-              elevation: 10,
-              child: SizedBox(
-                //color: Colors.grey,
-                height: 200,
-                width: 200,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      AspectRatio(
-                        aspectRatio: 2,
-                        child: Image.network(products.isEmpty
-                            ? loading[0]
-                            : products[index]["product-img"][0]),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        child: Text(
-                          '${products.isEmpty ? loading[1] : products[index]["product-name"]}',
-                          style: AppTextStyle.bodyTextStyle,
-                        ),
-                      ),
-                      Text(
-                        '${products.isEmpty ? loading[1] : products[index]["product-price"]}',
-                        style: AppTextStyle.bodyTextStyle,
-                      )
-                    ],
-                  ),
-                ),
+  Material imgSlider() {
+    return Material(
+      color: AppColor.appSecColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadiusDirectional.circular(10),
+      ),
+      elevation: 15,
+      child: FutureBuilder(
+        future: getSliderImg(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: AppColor.appMainColor,
               ),
             );
-          },
-        ),
+          } else {
+            return Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 5,
+                vertical: 15,
+              ),
+              child: Column(
+                children: [
+                  CarouselSlider.builder(
+                    itemCount: snapshot.data.length,
+                    options: CarouselOptions(
+                      height: 135,
+                      autoPlay: true,
+                      enlargeCenterPage: true,
+                      viewportFraction: .8,
+                      onPageChanged: (index, reason) => dotPosition = index,
+                    ),
+                    itemBuilder: (context, index, realIndex) {
+                      DocumentSnapshot data = snapshot.data[index];
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: AppColor.appSecColor,
+                          borderRadius: BorderRadius.circular(15),
+                          border: Border.all(
+                            color: AppColor.appMainColor,
+                            width: 2,
+                          ),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              data["img"],
+                            ),
+                            fit: BoxFit.fitWidth,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                  cDivider(15),
+                  AnimatedSmoothIndicator(
+                    activeIndex: dotPosition,
+                    count: snapshot.data.length,
+                    effect: const SwapEffect(
+                      activeDotColor: AppColor.appMainColor,
+                      dotColor: Colors.grey,
+                      dotHeight: 10,
+                      dotWidth: 10,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
 
-  Widget buildImg(String showImg, int index) => Container(
-        decoration: BoxDecoration(
-          color: AppColor.appSecColor,
-          borderRadius: BorderRadius.circular(15),
-          border: Border.all(
-            color: AppColor.appMainColor,
-            width: 2,
-          ),
-          image: DecorationImage(
-            image: NetworkImage(
-              showImg,
+  Material productList() {
+    return Material(
+      color: AppColor.appSecColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadiusDirectional.circular(10),
+      ),
+      elevation: 15,
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 10,
             ),
-            fit: BoxFit.fitWidth,
+            child: Text(
+              'Featured Products',
+              style: AppTextStyle.bodyTextStyle,
+            ),
           ),
-        ),
-      );
-
-  Widget buildIndicator() => AnimatedSmoothIndicator(
-        activeIndex: dotPosition,
-        count: imgForSlider.isEmpty ? 1 : imgForSlider.length,
-        effect: SwapEffect(
-          activeDotColor: AppColor.appMainColor,
-          dotColor: AppColor.appSecColor,
-          dotHeight: 10,
-          dotWidth: 10,
-        ),
-      );
+          Padding(
+            padding: const EdgeInsets.all(5).copyWith(top: 0),
+            child: FutureBuilder(
+              future: getProdacts(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(
+                      color: AppColor.appMainColor,
+                    ),
+                  );
+                } else {
+                  return SizedBox(
+                    height: 280,
+                    child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 1,
+                      ),
+                      itemCount: snapshot.data.length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (BuildContext context, int index) {
+                        DocumentSnapshot data = snapshot.data[index];
+                        return Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadiusDirectional.circular(15),
+                            side: const BorderSide(
+                                color: AppColor.appMainColor, width: 2),
+                          ),
+                          elevation: 0,
+                          child: Stack(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 5),
+                                child: Align(
+                                  alignment: Alignment.topCenter,
+                                  child: Container(
+                                    height: 160,
+                                    width: 160,
+                                    decoration: BoxDecoration(
+                                      color: AppColor.appSecColor,
+                                      borderRadius: BorderRadius.circular(15),
+                                      image: DecorationImage(
+                                        image: NetworkImage(
+                                          data["img"][0],
+                                        ),
+                                        // fit: BoxFit.fitWidth,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.centerLeft +
+                                    const Alignment(0, .6),
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 15, right: 10),
+                                  child: Text(
+                                    data["name"],
+                                    style: AppTextStyle.smallTextStyle,
+                                  ),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.bottomLeft +
+                                    const Alignment(0, -.1),
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                    left: 15,
+                                  ),
+                                  child: Text(
+                                    data["price"],
+                                    style: AppTextStyle.smallTextStyle,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
--- lib/Screen/bottom_nevigetion/wishlist.dart
+++ lib/Screen/bottom_nevigetion/wishlist.dart
@@ -1,17 +1,46 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dokan/Properties/export.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class Wishlist extends StatelessWidget {
+class Wishlist extends StatefulWidget {
   const Wishlist({Key? key}) : super(key: key);
 
   @override
+  State<Wishlist> createState() => _WishlistState();
+}
+
+class _WishlistState extends State<Wishlist> {
+  //
+
+  Future getSliderImg() async {
+    var firestore = FirebaseFirestore.instance;
+
+    QuerySnapshot qn = await firestore.collection('Slider').get();
+    return qn.docs;
+  }
+
+  //
+  Future getProdacts() async {
+    var firestore = FirebaseFirestore.instance;
+
+    QuerySnapshot qn = await firestore.collection('Products').get();
+    return qn.docs;
+  }
+
+  int dotPosition = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: AppColor.appBackground,
-      body: Center(
-        child: Text(
-          'Wishlist',
-          style: AppTextStyle.headerStyle,
-        ),
+      body: Column(
+        children: const [
+          Text(
+            'wish',
+            style: AppTextStyle.headerStyle,
+          ),
+        ],
       ),
     );
   }
